6861b8b9864e5117177a6fc2fdaa5fcc8a572400
diaoguangqiang <diaoguangqiang@126.com>
2017-02-09 13:35:56 +0800

    first commit

---
+++ SmartData/SmartData.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-01-28T16:29:27
+#
+#-------------------------------------------------
+
+QT       += core gui network
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = SmartData
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ SmartData/mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+//#include <QtNetwork>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    
+private:
+    //QNetworkAccessManager * m_manager_;
+
+private slots:
+    //void replyFinished( QNetworkReply * );
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
